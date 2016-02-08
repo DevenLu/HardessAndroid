bd028c88a99835e1955a357ebbd4632eea913499
markzhai <zhaiyifan56@gmail.com>
2016-02-08 22:23:55 +0800

    upadte code comment

--- domain/src/main/java/com/moduth/hardess/domain/executor/ThreadExecutor.java
+++ domain/src/main/java/com/moduth/hardess/domain/executor/ThreadExecutor.java
@@ -15,6 +15,8 @@
  */
 package com.moduth.hardess.domain.executor;
 
+import com.moduth.hardess.domain.interactor.UseCase;
+
 import java.util.concurrent.Executor;
 
 /**
@@ -23,7 +25,7 @@ import java.util.concurrent.Executor;
  *
  * Executor implementation can be based on different frameworks or techniques of asynchronous
  * execution, but every implementation will execute the
- * {@link com.ali.music.funzone.domain.interactor.UseCase} out of the UI thread.
+ * {@link UseCase} out of the UI thread.
  */
 public interface ThreadExecutor extends Executor {
 
